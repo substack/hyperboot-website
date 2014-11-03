7ec86d169fbcdae740e69d9450dee240eb885a77
James Halliday <mail@substack.net>
2014-11-03 12:12:55 -0800

    faster lightning, darker background

--- demo/app/main.js
+++ demo/app/main.js
@@ -2,6 +2,6 @@ document.querySelector('#click').addEventListener('click', function (ev) {
     var times = 0;
     var iv = setInterval(function () {
         document.body.classList.toggle('lightning');
-        if (++ times === 6) clearInterval(iv);
-    }, 200);
+        if (++ times === 12) clearInterval(iv);
+    }, 40);
 });
