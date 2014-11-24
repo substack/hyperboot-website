9ddd2b5d5c4eab9b0a4f3b28a74a75c34ee94e05
James Halliday <mail@substack.net>
2014-11-24 01:21:42 -0800

    updates for hyperboot 2.0.0

--- demo/app/main.js
+++ demo/app/main.js
@@ -1,7 +1,15 @@
-document.querySelector('#click').addEventListener('click', function (ev) {
+var hboot = require('hyperboot/rpc');
+
+var lightning = document.querySelector('#click');
+lightning.addEventListener('click', function (ev) {
     var times = 0;
     var iv = setInterval(function () {
         document.body.classList.toggle('lightning');
         if (++ times === 12) clearInterval(iv);
     }, 40);
+});
+
+var config = document.querySelector('#config');
+config.addEventListener('click', function (ev) {
+    hboot.toggle();
 });
--- demo/readme.markdown
+++ demo/readme.markdown
@@ -1,13 +1,24 @@
 # demo app
 
-To make a release, first install hyperboot and html-inline:
+## make a relase
+
+To make a release, first fetch the necessary dependencies:
 
 ```
-$ npm install hyperboot html-inline -g
+$ npm install .
 ```
 
 then do:
 
 ```
-$ html-inline app/index.html | hyperboot release -v 1.2.3 -m 'release notes...'
+$ npm run release -- -v 1.2.3 -m 'release notes...'
+```
+
+## upgrade hyperboot
+
+Install the version of the hyperboot command you want to push to clients, then
+do:
+
+```
+$ hyperboot upgrade
 ```
